5c388205d942a5c5848bb46cc80323777af00fe5
fabvest <fabvest7@gmail.com>
2017-11-27 17:50:39 +0300

    add some code

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,14 @@
+import static spark.Spark.*;
+
+
 public class Application {
     public static void main(String[] args) {
+        staticFileLocation("/public");
+
+        Integer port = 5555;
+
+        port(port);
+
 
     }
 }
