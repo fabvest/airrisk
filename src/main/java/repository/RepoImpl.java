fd2e8505d53e5bd66b8ee1e5c0b7065535a99221
fabvest <fabvest7@gmail.com>
2017-12-14 17:54:09 +0300

    add needed classes

--- src/main/java/repository/RepoImpl.java
+++ src/main/java/repository/RepoImpl.java
@@ -6,6 +6,8 @@ import org.hibernate.Transaction;
 import utils.HibernateUtil;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RepoImpl implements RepoInterface {
     public RepoImpl() {
@@ -33,5 +35,45 @@ public class RepoImpl implements RepoInterface {
                 s.close();
             }
         }
+    }
+
+    @Override
+    public Object getObject(Class clazz, Long id) throws SQLException {
+        Session s = null;
+        Transaction tx;
+        Object o = null;
+        try {
+            s = HibernateUtil.getSession();
+            tx = s.beginTransaction();
+            o = s.get(clazz, id);
+            tx.commit();
+        }catch (Exception e){
+            System.out.println("error with get event by id");
+        }finally {
+            if(s != null && s.isOpen()) {
+                s.close();
+            }
+        }
+        return o;
+    }
+
+    @Override
+    public List<Object> getAllObjects(Class clazz) throws SQLException {
+        Session session = null;
+        Transaction tx = null;
+        List ev = new ArrayList<>();
+        try {
+            session = HibernateUtil.getSession();
+            tx = session.beginTransaction();
+            ev = session.createCriteria(clazz).list();
+            tx.commit();
+        } catch (Exception e) {
+            System.out.println("error with get all ev types");
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
+        return ev;
     }
 }
--- src/main/java/repository/RepoInterface.java
+++ src/main/java/repository/RepoInterface.java
@@ -1,7 +1,10 @@
 package repository;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public interface RepoInterface <T> {
     boolean addObject(T emp) throws SQLException;
+    Object getObject(Class clazz, Long id) throws SQLException;
+    List getAllObjects(Class clazz) throws SQLException;
 }
