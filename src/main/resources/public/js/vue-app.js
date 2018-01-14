0f448f2aa1fc058e0124f75ee45ad410d42442be
Sergey Rozhkov <s.rozhkov@andersenlab.com>
2018-01-14 22:46:12 +0300

    Add validators

--- src/main/resources/public/js/vue-app.js
+++ src/main/resources/public/js/vue-app.js
@@ -1,3 +1,35 @@
+function CreateFormControl() {
+  // props
+  this.value = null;
+  this.isValid = false;
+  this.isShowErrorMessage = false;
+  var isTouch = false;
+  var isFocus = false;
+
+  // methods
+  this.onFocus = () => {
+    isTouch = isFocus = true;
+  };
+  this.onBlur = () => {
+    isFocus = false;
+    this.validator();
+  };
+  this.validator = () => {
+    this.isValid = !!this.value;
+    this.isShowErrorMessage = !this.isValid && !isFocus && isTouch;
+    return this.isValid;
+  };
+}
+
+function createWatchValue(nameControl) {
+  return function (newValue, oldValue) {
+    if (!oldValue && newValue) {
+      this[nameControl].isTouch = true;
+    }
+    this[nameControl].isValid = !!newValue;
+  };
+}
+
 var AutocompleteControlForm = {
   props: [ 'arrayObject', 'errorMessage' ],
   data : function () {
@@ -24,7 +56,10 @@ var AutocompleteControlForm = {
     },
     onTestValid: function () {
       if (!this.isTouch) return;
-      this.isValid = !!this.currentValue && this.currentValue.text === this.searchValue;
+      if (this.currentValue && this.currentValue.text !== this.searchValue) {
+        this.currentValue = null;
+      }
+      this.isValid = !!this.currentValue;
     },
     onBlurInput: function () {
       setTimeout(() => {
@@ -37,6 +72,9 @@ var AutocompleteControlForm = {
     },
     showErrorMessage: function () {
       return !this.isValid && this.searchValue.length && !this.isFocus && this.isTouch;
+    },
+    onSubmit: function () {
+      return this.currentValue;
     }
   },
   template: `
@@ -69,8 +107,8 @@ var AutocompleteControlForm = {
 var DrugControlForm = {
   data () {
     return {
-      unit: null,
-      concentration: null,
+      unitControl: new CreateFormControl('unitControl'),
+      concentrationControl: new CreateFormControl('concentrationControl'),
       arrayDrug: [
         { id: 1, text: 'Морфий' },
         { id: 2, text: 'Свинец' },
@@ -84,15 +122,23 @@ var DrugControlForm = {
       errorMessageDrug: 'Вы не выбрали вещество.'
     }
   },
+  watch: {
+    'unitControl.value': createWatchValue('unitControl'),
+    'concentrationControl.value': createWatchValue('concentrationControl')
+  },
   methods: {
     onRemoveForm () {
       this.$emit('removeDrug');
     },
-    submit () {
+    isValidControls() {
+      return this.unitControl.value && this.concentrationControl.value && this.$refs['drug-autocomplete'].onSubmit();
+    },
+    submit() {
+      if (!this.isValidControls()) return null;
       return {
-        drug: this.$refs['drug-autocomplete'].currentValue.id,
-        unit: this.unit,
-        concentration: Number(this.concentration)
+        drug: this.$refs['drug-autocomplete'].onSubmit().id,
+        unit: this.unitControl.value,
+        concentration: Number(this.concentrationControl.value)
       }
     }
   },
@@ -107,14 +153,34 @@ var DrugControlForm = {
   </div>
   <div class="form-group row">
     <label class="col-md-5" for="unit">Единица измерения</label>
-    <select v-model="unit" id="unit" class="form-control col-md-7" required>
+    <select
+    v-model="unitControl.value"
+    v-on:focus="unitControl.onFocus()"
+    v-on:blur="unitControl.onBlur()"
+    v-bind:class="{ 'is-valid': unitControl.isValid, 'is-invalid': unitControl.isShowErrorMessage }"
+    class="form-control col-md-7"
+    id="unit"
+    required
+    >
       <option selected disabled>Выберите...</option>
       <option v-for="unit in arrayUnit" v-bind:value="unit.id">{{ unit.text }}</option>
     </select>
+    <p v-bind:class="{ 'display-block': unitControl.isShowErrorMessage }" class="invalid-feedback">
+      Вы не выбрали единицы измерения.
+    </p>
   </div>
   <div class="form-group row">
     <label class="col-md-5" for="concentration">Концентрация вещества</label>
-    <input v-model="concentration" id="concentration" class="form-control col-md-7" type="number" required>
+    <input
+    v-model="concentrationControl.value"
+    v-on:focus="concentrationControl.onFocus()"
+    v-on:blur="concentrationControl.onBlur()"
+    v-bind:class="{ 'is-valid': concentrationControl.isValid, 'is-invalid': concentrationControl.isShowErrorMessage }"
+    class="form-control col-md-7"
+    id="concentration"
+    type="number"
+    required
+    >
   </div>
   <button v-on:click="onRemoveForm()" class="btn btn-primary" type="button">Удалить вещество</button>
   <hr>
@@ -125,17 +191,17 @@ new Vue({
   el: '#v-form',
   data () {
     return {
-      org: null,
-      city: null,
-      district: null,
-      street: null,
-      house: null,
-      catPeople: null,
-      startPerMonth: null,
-      startPerYear: null,
-      endPerMonth: null,
-      endPerYear: null,
-      date: null,
+      orgControl: new CreateFormControl('orgControl'),
+      cityControl: new CreateFormControl('cityControl'),
+      districtControl: new CreateFormControl('districtControl'),
+      streetControl: new CreateFormControl('streetControl'),
+      houseControl: new CreateFormControl('houseControl'),
+      catPeopleControl: new CreateFormControl('catPeopleControl'),
+      startPerMonthControl: new CreateFormControl('startPerMonthControl'),
+      startPerYearControl: new CreateFormControl('startPerYearControl'),
+      endPerMonthControl: new CreateFormControl('endPerMonthControl'),
+      endPerYearControl: new CreateFormControl('endPerYearControl'),
+      dateControl: new CreateFormControl('dateControl'),
       arrayOrg: [
         'Роспотребнадзор по Пензе',
         'Роспотребнадзор по Самаре'
@@ -149,6 +215,19 @@ new Vue({
       arrayMonth: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябь', 'Ноябрь', 'Декабрь']
     }
   },
+  watch: {
+    'orgControl.value': createWatchValue('orgControl'),
+    'cityControl.value': createWatchValue('cityControl'),
+    'districtControl.value': createWatchValue('districtControl'),
+    'streetControl.value': createWatchValue('streetControl'),
+    'houseControl.value': createWatchValue('houseControl'),
+    'catPeopleControl.value': createWatchValue('catPeopleControl'),
+    'startPerMonthControl.value': createWatchValue('startPerMonthControl'),
+    'startPerYearControl.value': createWatchValue('startPerYearControl'),
+    'endPerMonthControl.value': createWatchValue('endPerMonthControl'),
+    'endPerYearControl.value': createWatchValue('endPerYearControl'),
+    'dateControl.value': createWatchValue('dateControl')
+  },
   methods: {
     addDrug () {
       this.arraySelectForm.push(this.arraySelectForm.length);
@@ -159,7 +238,7 @@ new Vue({
     postDataForm (data) {
       this.$http.post('/getinfo', data).then(
         (response) => console.log(response), // successful
-        (responseError) => console.error(responseError) // error
+        (error) => console.error(error) // error
       );
     },
     submit () {
@@ -172,18 +251,19 @@ new Vue({
       });
       formValue.arrayDrag = arrayDrag;
       // Set location info
-      formValue.org = this.org;
-      formValue.city = this.city;
-      formValue.district = this.district;
-      formValue.street = this.street;
-      formValue.house = Number(this.house);
-      formValue.catPeople = this.catPeople;
-      formValue.startPerMonth = Number(this.startPerMonth + 1);
-      formValue.startPerYear = Number(this.startPerYear);
-      formValue.endPerMonth = Number(this.endPerMonth + 1);
-      formValue.endPerYear = Number(this.endPerYear);
-      formValue.date = this.date;
+      formValue.orgControl = this.orgControl.value;
+      formValue.cityControl = this.cityControl.value;
+      formValue.districtControl = this.districtControl.value;
+      formValue.streetControl = this.streetControl.value;
+      formValue.houseControl = Number(this.houseControl.value);
+      formValue.catPeopleControl = this.catPeopleControl.value;
+      formValue.startPerMonthControl = Number(this.startPerMonthControl.value) + 1;
+      formValue.startPerYearControl = Number(this.startPerYearControl.value);
+      formValue.endPerMonthControl = Number(this.endPerMonthControl.value) + 1;
+      formValue.endPerYearControl = Number(this.endPerYearControl.value);
+      formValue.dateControl = this.dateControl.value;
       // post data
+      console.log(_.clone(formValue));
       this.postDataForm(formValue)
     }
   },
@@ -195,62 +275,152 @@ new Vue({
     <h2>Место нахождения</h2>
     <div class="form-group row">
       <label class="col-md-5">Организация</label>
-      <select v-model="org" class="form-control col-md-7" required>
+      <select
+        v-model="orgControl.value"
+        v-on:focus="orgControl.onFocus()"
+        v-on:blur="orgControl.onBlur()"
+        v-bind:class="{ 'is-invalid': orgControl.isShowErrorMessage, 'is-valid': orgControl.isValid }"
+        class="form-control col-md-7"
+        required
+      >
         <option selected disabled>Выберите...</option>
         <option v-for="org in arrayOrg" v-bind:value="org">{{ org }}</option>
       </select>
     </div>
     <div class="form-group row">
       <label class="col-md-5" for="city">Город</label>
-      <input v-model="city" id="city" class="form-control col-md-7" type="text" required>
+      <input
+        v-model="cityControl.value"
+        v-on:focus="cityControl.onFocus()"
+        v-on:blur="cityControl.onBlur()"
+        v-bind:class="{ 'is-invalid': cityControl.isShowErrorMessage, 'is-valid': cityControl.isValid }"
+        class="form-control col-md-7"
+        id="city"
+        type="text"
+        required
+        >
     </div>
     <div class="form-group row">
       <label class="col-md-5" for="district">Район</label>
-      <input v-model="district" id="district" class="form-control col-md-7" type="text" required>
+      <input
+        v-model="districtControl.value"
+        v-on:focus="districtControl.onFocus()"
+        v-on:blur="districtControl.onBlur()"
+        v-bind:class="{ 'is-invalid': districtControl.isShowErrorMessage, 'is-valid': districtControl.isValid }"
+        class="form-control col-md-7"
+        id="district"
+        type="text"
+        required
+      >
     </div>
     <div class="form-group row">
       <label class="col-md-5" for="street">Улица</label>
-      <input v-model="street" id="street" class="form-control col-md-7" type="text" required>
+      <input
+        v-model="streetControl.value"
+        v-on:focus="streetControl.onFocus()"
+        v-on:blur="streetControl.onBlur()"
+        v-bind:class="{ 'is-invalid': streetControl.isShowErrorMessage, 'is-valid': streetControl.isValid }"
+        class="form-control col-md-7"
+        id="street"
+        type="text"
+        required
+      >
     </div>
     <div class="form-group row">
       <label class="col-md-5" for="house">Дом</label>
-      <input v-model="house" id="house" class="form-control col-md-7" type="number" required>
+      <input
+        v-model="houseControl.value"
+        v-on:focus="houseControl.onFocus()"
+        v-on:blur="houseControl.onBlur()"
+        v-bind:class="{ 'is-invalid': houseControl.isShowErrorMessage, 'is-valid': houseControl.isValid }"
+        class="form-control col-md-7"
+        id="house"
+        type="number"
+        required
+      >
     </div>
     <div class="form-group row">
       <label class="col-md-5" for="category">Категория населения</label>
-      <select v-model="catPeople" id="category" class="form-control col-md-7" required>
+      <select
+        v-model="catPeopleControl.value"
+        v-on:focus="catPeopleControl.onFocus()"
+        v-on:blur="catPeopleControl.onBlur()"
+        v-bind:class="{ 'is-invalid': catPeopleControl.isShowErrorMessage, 'is-valid': catPeopleControl.isValid }"
+        class="form-control col-md-7"
+        id="category"
+        required
+      >
         <option selected disabled>Выберите...</option>
         <option v-for="cat in arrayCatPeople" v-bind:value="cat.value">{{ cat.text }}</option>
       </select>
     </div>
     <hr>
-    <div class="container row">
+    <div class="container row title-group">
       <h2>Вещества</h2>
-      <button v-on:click="addDrug()" type="button" class="btn btn-primary" style="margin-left: 20px;">Добавить вещество</button>
+      <button v-on:click="addDrug()" type="button" class="btn btn-primary title-button">Добавить вещество</button>
     </div>
     <v-drug-control v-for="index in arraySelectForm" @removeDrug="removeDrugIndex(index)" ref="customSelect"></v-drug-control>
     <h2>Период</h2>
     <div class="form-group row">
-      <!-- Месяц -->
       <label class="col-md-5">Начало периода</label>
-      <select v-model="startPerMonth" id="start_per_unit" class="form-control col-md-3" required>
+      <select
+        v-model="startPerMonthControl.value"
+        v-on:focus="startPerMonthControl.onFocus()"
+        v-on:blur="startPerMonthControl.onBlur()"
+        v-bind:class="{ 'is-invalid': startPerMonthControl.isShowErrorMessage, 'is-valid': startPerMonthControl.isValid }"
+        id="start_per_unit"
+        class="form-control col-md-3"
+        required
+      >
         <option selected disabled>Выберите месяц...</option>
         <option v-for="(month, index) in arrayMonth" v-bind:value="index">{{ month }}</option>
       </select>
-      <!-- Год -->
-      <input v-model="startPerYear" type="number" class="form-control col-md-3 offset-md-1" placeholder="Введите год...">
+      <input
+        v-model="startPerYearControl.value"
+        v-on:focus="startPerYearControl.onFocus()"
+        v-on:blur="startPerYearControl.onBlur()"
+        v-bind:class="{ 'is-invalid': startPerYearControl.isShowErrorMessage, 'is-valid': startPerYearControl.isValid }"
+        class="form-control col-md-3 offset-md-1"
+        type="number"
+        placeholder="Введите год..."
+        >
     </div>
     <div class="form-group row">
       <label class="col-md-5">Конец периода</label>
-      <select v-model="endPerMonth" id="end_per_unit" class="form-control col-md-3" required>
+      <select
+        v-model="endPerMonthControl.value"
+        v-on:focus="endPerMonthControl.onFocus()"
+        v-on:blur="endPerMonthControl.onBlur()"
+        v-bind:class="{ 'is-invalid': endPerMonthControl.isShowErrorMessage, 'is-valid': endPerMonthControl.isValid }"
+        class="form-control col-md-3"
+        id="end_per_unit"
+        required
+      >
         <option selected disabled>Выберите месяц...</option>
         <option v-for="(month, index) in arrayMonth" v-bind:value="index">{{ month }}</option>
       </select>
-      <input v-model="endPerYear" type="number" class="form-control col-md-3 offset-md-1" placeholder="Введите год...">
+      <input
+        v-model="endPerYearControl.value"
+        v-on:focus="endPerYearControl.onFocus()"
+        v-on:blur="endPerYearControl.onBlur()"
+        v-bind:class="{ 'is-invalid': endPerYearControl.isShowErrorMessage, 'is-valid': endPerYearControl.isValid }"
+        class="form-control col-md-3 offset-md-1"
+        type="number"
+        placeholder="Введите год..."
+      >
     </div>
     <div class="form-group row">
       <label class="col-md-5" for="date">Дата</label>
-      <input type="date" v-model="date" id="date" class="form-control col-md-7" required>
+      <input
+        v-model="dateControl.value"
+        v-on:focus="dateControl.onFocus()"
+        v-on:blur="dateControl.onBlur()"
+        v-bind:class="{ 'is-invalid': dateControl.isShowErrorMessage, 'is-valid': dateControl.isValid }"
+        class="form-control col-md-7"
+        type="date"
+        id="date"
+        required
+      >
     </div>
     <div>
       <button v-on:click="submit()" type="button" class="btn btn-primary btn-lg btn-block">Рассчитать</button>
