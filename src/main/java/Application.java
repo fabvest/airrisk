b43f7fa62ffe81bd8f5dac4c982c222cdea68a31
fabvest <fabvest7@gmail.com>
2018-01-17 12:52:00 +0300

    fuck

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -5,9 +5,19 @@ public class Application {
     public static void main(String[] args) {
         staticFileLocation("/public");
 
-        Integer port = 5555;
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 5555;
+        }
 
         port(port);
+
+//        Integer port = 5555;
+//
+//        port(port);
 
         new ClientRoutes();
     }
